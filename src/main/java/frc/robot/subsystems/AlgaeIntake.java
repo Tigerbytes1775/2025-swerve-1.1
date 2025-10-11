a2077480903b1415095e33efa58bb8f79cd6af50
Tigerbytes 1775 Team Account <tigerbytes1775@gmail.com>
2025-10-11 13:40:52 -0500

    Encoders, and PID tested(need slight change), almost cowtown ready

--- src/main/java/frc/robot/subsystems/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntake.java
@@ -15,7 +15,7 @@ public class AlgaeIntake extends SubsystemBase {
     
     private final SparkMax algaeMotor = new SparkMax(31, SparkLowLevel.MotorType.kBrushless);
 
-    double algeaStrength = 0.05;
+    double algeaStrength = 0.25;
     
     
     public AlgaeIntake() {}
